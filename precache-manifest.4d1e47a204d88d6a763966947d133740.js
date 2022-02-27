2d15bfad61f438978aa301a2105ed23fc9e324dc
Sarthak <sarthakrajjindal44@gmail.com>
2022-02-27 17:00:47 +0530

    Updates

---
+++ precache-manifest.4d1e47a204d88d6a763966947d133740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f05ac8c4846631edf11f0fff3588a1",
+    "url": "/Plan-Organisation-Tool/index.html"
+  },
+  {
+    "revision": "cf1fbf88b5d674d35d3f",
+    "url": "/Plan-Organisation-Tool/static/css/main.0146a28c.chunk.css"
+  },
+  {
+    "revision": "85c745943c906f51e663",
+    "url": "/Plan-Organisation-Tool/static/js/2.05ffd95e.chunk.js"
+  },
+  {
+    "revision": "49a5c558fb8d5ae891f0438137628f91",
+    "url": "/Plan-Organisation-Tool/static/js/2.05ffd95e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf1fbf88b5d674d35d3f",
+    "url": "/Plan-Organisation-Tool/static/js/main.2cd882c2.chunk.js"
+  },
+  {
+    "revision": "c519bb98e5966d0213f9",
+    "url": "/Plan-Organisation-Tool/static/js/runtime-main.e6af9369.js"
+  }
+]);
